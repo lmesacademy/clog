4e92bc3c047340d8cd95d74bc883503f777f1479
Jacob Samro <jacobsamro@outlook.com>
2024-12-13 00:28:58 +0530

    fix(example): get logtail token from env

--- example/index.ts
+++ example/index.ts
@@ -1,9 +1,9 @@
 import { Clog } from '../src';
 
-Clog.init('', { singleLine: false });
+Clog.init(process.env.LOGTAIL_TOKEN, { singleLine: true });
 
 console.log('Hello World');
 
 console.info(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-console.warn('warning', { a: 1, b: 2, c: 3 });
+console.error('warning', { a: 1, b: 2, c: 3 });
